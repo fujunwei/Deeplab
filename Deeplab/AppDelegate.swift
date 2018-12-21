5dbe3ff058661a7552a3423ea2241ab94813e5d0
mac-webgl-stable <mac-webgl-stable@mac-webgl-stables-MacBook-Pro.local>
2018-12-21 19:29:55 +0800

    first commit

---
+++ Deeplab/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Deeplab
+//
+//  Created by mac-webgl-stable on 12/21/18.
+//  Copyright © 2018 mac-webgl-stable. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Deeplab/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Deeplab
+//
+//  Created by mac-webgl-stable on 12/21/18.
+//  Copyright © 2018 mac-webgl-stable. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
